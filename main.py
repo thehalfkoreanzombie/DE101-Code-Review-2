75bf991a4ca2cdecfd2f194f4a11f8dc5f83848f
thehalfkoreanzombie <thehalfkoreanzombie@gmail.com>
2023-10-18 15:27:18 -0700

    Plus three commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def plus_three(number_1): #set up one argument in the function
+    number_plus_three = number_1 + 3 #set variable to add 3 to the argument
+    return(number_plus_three) #return the value after function runs)
+
+print(plus_three(5)) #first call
+print(plus_three(10)) #second call
